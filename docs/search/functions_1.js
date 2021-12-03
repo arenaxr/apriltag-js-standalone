17e19dae39bde3bb4057283f915141065a60f74f
nampereira <3504501+nampereira@users.noreply.github.com>
2021-12-03 13:39:45 +0000

    Deploying to gh-pages from @ conix-center/apriltag-js-standalone@202f52795f4e134004599f83699797d437d4c03b 🚀

--- docs/search/functions_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['str_5fjson_5fclear',['str_json_clear',['../str__json_8c.html#accf0b31fe42caf50bad0d06e7c584b1f',1,'str_json_clear(t_str_json *str_json):&#160;str_json.c'],['../str__json_8h.html#accf0b31fe42caf50bad0d06e7c584b1f',1,'str_json_clear(t_str_json *str_json):&#160;str_json.c']]],
-  ['str_5fjson_5fconcat',['str_json_concat',['../str__json_8c.html#ac13a475167fb53407135c2aa4e2d6483',1,'str_json_concat(t_str_json *str_json_dest, const char *source_c_str):&#160;str_json.c'],['../str__json_8h.html#ac13a475167fb53407135c2aa4e2d6483',1,'str_json_concat(t_str_json *str_json_dest, const char *source_c_str):&#160;str_json.c']]],
-  ['str_5fjson_5fcreate',['str_json_create',['../str__json_8c.html#a1c9c32b288150ce960673fcad588cfce',1,'str_json_create(t_str_json *str_json, size_t size_bytes):&#160;str_json.c'],['../str__json_8h.html#a1c9c32b288150ce960673fcad588cfce',1,'str_json_create(t_str_json *str_json, size_t size_bytes):&#160;str_json.c']]],
-  ['str_5fjson_5fdestroy',['str_json_destroy',['../str__json_8c.html#a761affea5aa75ade34bb8bd442e82daf',1,'str_json_destroy(t_str_json *str_json):&#160;str_json.c'],['../str__json_8h.html#a761affea5aa75ade34bb8bd442e82daf',1,'str_json_destroy(t_str_json *str_json):&#160;str_json.c']]],
-  ['str_5fjson_5fprintf',['str_json_printf',['../str__json_8c.html#a0b9364d740330a643eba3080de513827',1,'str_json_printf(t_str_json *str_json_dest, const char *format,...):&#160;str_json.c'],['../str__json_8h.html#a0b9364d740330a643eba3080de513827',1,'str_json_printf(t_str_json *str_json_dest, const char *format,...):&#160;str_json.c']]]
-];
